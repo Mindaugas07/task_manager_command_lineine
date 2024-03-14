ca44d9cfb2a903793b9decca201b8ae19008a935
Mindaugas Minelga <minelga.m@gmail.com>
2024-03-14 17:45:09 +0200

    add main functionality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
+import datetime, os
 from random import randint
-from faker import Faker
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from typing import Dict, List, Union
@@ -41,26 +40,12 @@ class MongoDB:
 
     def insert_many_document(self, document: Dict) -> List[str]:
         result = self.collection.insert_many(document)
-        print(f"Printed result: {result}")
+        # print(f"Printed result: {result}")
         return list(result.inserted_ids)
 
-    def create_task(self) -> str:
-        title = fake.first_name()
-        description = fake.last_name()
-
-        document = {
-            "title": title,
-            "description": description,
-        }
-        result = self.collection.insert_one(document)
-        print(f"Inserted document with ID: {result.inserted_id}")
-        print(f"This person was inserted into the database: {document}")
-
-        return str(result.inserted_id)
-
-    def generate_data_base(self, numb_of_documents):
-        for _ in range(numb_of_documents):
-            self.create_random_person()
+    # def generate_data_base(self, numb_of_documents):
+    #     for _ in range(numb_of_documents):
+    #         self.create_random_person()
 
     def query_equal(
         self, field_name: str, value: Union[str, int, float, bool], parameter: Dict = {}
@@ -119,12 +104,97 @@ class MongoDB:
         return list(result)
 
 
+def show_app_menu() -> str:
+
+    return input(
+        f"""
+If you want to add a new task press {"--1--":^35}
+If you want to view all tasks press {"--2--":^35} 
+
+Press --9-- to quit our app                          
+"""
+    )
+
+
 if __name__ == "__main__":
-    mongodb = MongoDB(
+    taskdb = MongoDB(
         host="localhost",
         port=27017,
-        db_name="task manager",
+        db_name="task_manager",
         collection_name="tasks",
     )
 
-    
+    os.system("cls")
+
+    while True:
+        try:
+            user_option = show_app_menu()
+        except:
+            print("Wrong input. Please enter a number from the list...")
+            break
+        if user_option == "1":
+            os.system("cls")
+            title = input("Enter the title of the task: ")
+            description = input("Enter the task descripiton: ")
+            status = "In progress"
+            document = {"title": title, "description": description, "status": status}
+            taskdb.insert_one_document(document)
+            os.system("cls")
+            print(f"Your task '{title}' was created!")
+
+        elif user_option == "2":
+            os.system("cls")
+            query = {}
+            task_details = taskdb.find_documents(query)
+            print("Your tasks:")
+            for index, task in enumerate(task_details):
+                index += 1
+                print(
+                    f"Task nr. {index}.-- {task['title']}. Description: {task['description']}. Status: {task['status']}"
+                )
+            print()
+            user_option = input(
+                """Enter the number of the task which you wand to delete: 
+                
+Press --9-- to go back
+"""
+            )
+            if user_option == "9":
+                os.system("cls")
+
+                user_option = show_app_menu()
+
+            else:
+                selected_document = task_details[int(user_option) - 1]
+                query = {"_id": selected_document["_id"]}
+                taskdb.delete_one_documents(query)
+                os.system("cls")
+                print(f"Task '{task['title']}' was deleted!")
+
+        #     print(user_table_option)
+        #     if user_table_option == "1":
+        #         jazz_place.reserve_table(int(user_table_option))
+        #         os.system("cls")
+        #         user_options()
+
+        #     elif user_table_option == "2":
+        #         jazz_place.reserve_table(int(user_table_option))
+        #         user_options()
+
+        #     elif user_table_option == "9":
+        #         os.system("cls")
+        #         print(jazz_place)
+        #         user_options()
+
+        # elif user_option == "3":
+        #     os.system("cls")
+        #     print(jazz_place)
+        #     jazz_place.reserve_table(1)
+
+        # elif user_option == "4":
+        #     os.system("cls")
+        #     print(jazz_place)
+
+        elif user_option == "9":
+            os.system("cls")
+            break
